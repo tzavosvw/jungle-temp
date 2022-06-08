f7d750c8764897e7b4af5dc5a89a6eb98488984a
Theodoros Zavos <theodoros_z@hotmail.com>
2022-06-08 21:52:15 +0300

    initial commit - coming soon only

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
+import logo from './coming-soon.webp';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App1">
+      <header className="App-header1">
+        <img src={logo} className="App-logo1" alt="logo" />
       </header>
     </div>
   );
