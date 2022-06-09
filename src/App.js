04b88e6737e373f6d2c478b5bfe9175b6431973e
Theodoros Zavos <tzavos@vivawallet.com>
2022-06-09 12:09:30 +0300

    add new css changes for mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import logo from './coming-soon.webp';
 import './App.css';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function App() {
+const isDesktop = useMediaQuery('(min-width: 1080px)');
+
   return (
     <div className="App1">
       <header className="App-header1">
-        <img src="coming-soon.webp" className="App-logo1" alt="logo" />
+      {isDesktop ? <img src="coming-soon.webp" className="App-logo1" alt="logo" /> : <span></span>}
+        
       </header>
     </div>
   );
